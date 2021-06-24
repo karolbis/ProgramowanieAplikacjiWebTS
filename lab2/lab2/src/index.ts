fab11f2bc15b0f2cf68fe6aebc5b51693f7ea1e2
karolbis <61646274+karolbis@users.noreply.github.com>
2021-06-24 20:59:03 +0200

    Update index.ts

--- lab2/lab2/src/index.ts
+++ lab2/lab2/src/index.ts
@@ -1,39 +1,113 @@
-const channel1: any[] = [];
-const clapAudio: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
-
-const playChannel1Btn: HTMLButtonElement = document.querySelector('#playChannel1');
-
-document.body.addEventListener('keypress', onKeyDown);
-playChannel1Btn.addEventListener('click', onPlayChannel1);
-
-function onKeyDown(ev: KeyboardEvent): void {
-
-    const key = ev.key;
-    const time = ev.timeStamp;
-
-    channel1.push({
-        key,
-        time
-    });
-
-    playSound(key);
-    console.log(channel1);
+interface SoundData {
+    key: string;
+    time: number;
+}
+interface ChannelsContainer {
+    [key: string]: SoundData[];
 }
 
-function playSound(key: string) {
-    
-    clapAudio.currentTime = 0;
-    clapAudio.play();
+class SoundPlayer {
+    private audioElements!: HTMLAudioElement[];
+    private dataChannels!: HTMLDivElement[];
+    private channelContainer: ChannelsContainer;
+    private activeChannels: Set<string>;
+    constructor() {
+        this.getElements();
+        this.addEventListeners();
+        this.activeChannels = new Set();
+        this.channelContainer = this.dataChannels.reduce(
+            (acc, { dataset }) => ({ ...acc, [dataset.channel as string]: [] }),
+            {},
+        );
+    }
+
+    getElements() {
+        this.audioElements = [...document.querySelectorAll('audio')];
+        this.dataChannels = [...document.querySelectorAll('[data-channel]')] as HTMLDivElement[];
+    }
+
+    addEventListeners() {
+        window.addEventListener('keydown', e => this.handleKeyEvent(e));
+
+        this.dataChannels.forEach(channel =>
+            channel.addEventListener('click', e => this.handleChannelClick(e)),
+        );
+    }
+
+    handleKeyEvent(e: KeyboardEvent) {
+        const { key, timeStamp: time } = e;
+
+        if (this.activeChannels.size) 
+        {
+            this.activeChannels.forEach(channelId => {
+                this.channelContainer[channelId] = [...this.channelContainer[channelId],
+                { key, time },
+                ];
+            });
+        }
+
+        this.playSound(key);
+    }
+
+    playSound(key: string) {
+        const audio = this.audioElements.find(({ dataset }) => dataset.soundKey === key);
+
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    handleChannelClick(e: Event) {
+        const { dataset: parentDataset } = e.currentTarget as HTMLDivElement;
+        const button = e.target as HTMLButtonElement;
+
+        if (button.className.includes('play')) {
+            this.playChannel(parentDataset.channel);
+            return;
+        }
+
+        this.setActiveChannel(parentDataset.channel);
+    }
+
+    playChannel(channelId?: string) {
+        if (!channelId) return;
+
+        this.channelContainer[channelId].forEach(({ key, time }) =>
+            setTimeout(() => this.playSound(key), time),
+        );
+    }
+
+    setActiveChannel(channelId?: string) {
+        if (!channelId) return;
+
+        if (this.isChannelActive(channelId)) return;
+
+        this.activeChannels.add(channelId);
+        this.changeRecordButtonText(channelId, 'Stop');
+    }
+
+    isChannelActive(channelId: string) {
+        const isActive = this.activeChannels.has(channelId);
+
+        if (isActive) {
+            this.activeChannels.delete(channelId);
+            this.changeRecordButtonText(channelId, 'Record');
+
+            return true;
+        }
+
+        return false;
+    }
+
+    changeRecordButtonText(channelId: string, text: string) {
+        const recordButton = (this.dataChannels.find(
+            ({ dataset }) => dataset.channel === channelId,
+        ) as HTMLDivElement).firstElementChild as HTMLButtonElement;
+
+        recordButton.textContent = text;
+    }
+
+
 }
 
-function onPlayChannel1(): void {
-    playChannel();
-}
-
-function playChannel(): void {
-    let prevTime = 0;
-    channel1.forEach(sound => {
-        const timeout = sound.time - prevTime;
-        setTimeout(() => playSound(sound.key), timeout);
-    });
-}
+const sp = new SoundPlayer();
